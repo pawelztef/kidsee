bbac500099da1c279d421b3f55a2bed26ca182ab
Pavel <pawelztef@gmail.com>
2019-04-21 21:56:40 +0100

    SETTINGS MODEL UPDATE Adding singelton_guard integer field to settings table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_200943) do
+ActiveRecord::Schema.define(version: 2019_04_21_204658) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_04_21_200943) do
     t.string "catch_phrase"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "singleton_guard", default: 0
   end
 
 end
