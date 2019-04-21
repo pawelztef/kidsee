267ae8a0f83a1568e62eeebaef3055f6f9e2e6e2
Pavel <pawelztef@gmail.com>
2019-04-21 20:37:22 +0100

    models admin & parent migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_21_193640) do
+
+  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "second_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "second_name"
+    t.string "email"
+    t.string "phone_1"
+    t.string "phone_2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
