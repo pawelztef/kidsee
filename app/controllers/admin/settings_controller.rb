deb7ff498223e57ef590e26623c65a6b9d9d0362
Pavel <pawelztef@gmail.com>
2019-04-21 21:28:01 +0100

    SETTINGS CONTROLLER UPDATE Actions update and edit as well private methods customized.

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,7 +1,20 @@
 class Admin::SettingsController < ApplicationController
-  def update
-  end
+  before_action :set_title
 
   def edit
+    @setting = Admin::Setting.instance
+  end
+
+  def update
+    @setting = Admin::Setting.update(settings_params)
+    redirect_to admin_settings_edit_url
+  end
+
+  private
+  def set_title
+    @title = "Settings"
+  end
+  def settings_params
+    params.require(:admin_setting).permit(:company_name)
   end
 end
