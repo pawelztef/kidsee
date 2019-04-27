9f473f67738d627ecb9d88116beb2b4b48762e6c
Pavel <pawelztef@gmail.com>
2019-04-27 10:29:28 +0100

    REDIRECT METHOD CREATED Private method added to application controller. Method redirect client back to login page after clinet sing out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+ private
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      new_user_session_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    end
+  end 
 end
