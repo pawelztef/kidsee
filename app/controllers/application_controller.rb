9cd610773d03697415a6211afed3d06fe7512dbd
Pavel <pawelztef@gmail.com>
2019-04-27 11:40:35 +0100

    REDIRECTION AFTER SING IN AND SIGN OUT Implemented throught routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::Base
   #TODO move contnent to proper controller
   before_action :authenticate_admin!
  private
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :user
-      new_user_session_path
-    elsif resource_or_scope == :admin
-      new_admin_session_path
-    end
-  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :admins
+  devise_scope :admin do
+    authenticated  do
+      root to: 'admins/settings#index', as: 'authenticated_admin_root'
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: 'unauthenticated_admin_root'
+    end
+  end
   namespace :admins do
-    root to: 'settings#index'
     resources :settings
   end
 end
