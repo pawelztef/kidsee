9f17b8235fde5e9403b8abf0037c41eb23785fc8
Pavel <pawelztef@gmail.com>
2019-04-28 13:02:39 +0100

    DEVISE CONTROLLERS OVERRIDING

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :caregivers
+
+  # devise_for :caregivers
+  devise_for :caregivers, controllers: {
+    sessions: 'caregivers/sessions'
+  }
+
   devise_scope :caregiver do
     authenticated  do
       root to: 'caregivers/caregivers#index', as: 'authenticated_caregiver_root'
@@ -9,7 +14,10 @@ Rails.application.routes.draw do
   namespace :caregivers do
   end
 
-  devise_for :admins
+  # devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   devise_scope :admin do
     authenticated  do
       root to: 'admins/settings#index', as: 'authenticated_admin_root'
