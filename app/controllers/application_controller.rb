ddd866c45d3f2fe32405d0ec092d33c84a627964
Pavel <pawelztef@gmail.com>
2019-04-28 12:01:51 +0100

    SETTINGS IN INITIALIZER Set scoped devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
- layout :layout_by_controller
- protected
-   def layout_by_controller
-      if devise_controller? && resource_name == :admin
-        "admin_authentication"
-      elsif devise_controller? && resource_name == :caregiver
-        "front_authentication" 
-      end
-   end
+  layout :layout_by_controller
+  protected
+  def layout_by_controller
+    if devise_controller? && resource_name == :admin
+      "admin_authentication"
+    elsif devise_controller? && resource_name == :caregiver
+      "front_authentication" 
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
     authenticated  do
       root to: 'caregivers/caregivers#index', as: 'authenticated_caregiver_root'
     end
-
-    unauthenticated do
-      root to: 'devise/sessions#new', as: 'unauthenticated_caregiver_root'
-    end
   end
 
   namespace :caregivers do
@@ -17,10 +13,6 @@ Rails.application.routes.draw do
   devise_scope :admin do
     authenticated  do
       root to: 'admins/settings#index', as: 'authenticated_admin_root'
-    end
-
-    unauthenticated do
-      root to: 'devise/sessions#new', as: 'unauthenticated_admin_root'
     end
   end
 
