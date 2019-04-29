b0290d1697bf220fe4647e690e66647ee93c4e70
Pavel <pawelztef@gmail.com>
2019-04-29 08:07:35 +0100

    DEVISE AFTER SIGN OUT PATH METHOD OVERRIDED Request.referrer added to method to redirect client where it came from.

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,11 @@
 Rails.application.routes.draw do
 
-  # devise_for :caregivers
-  devise_for :caregivers, controllers: {
-    sessions: 'caregivers/sessions'
-  }
+  root to: 'welcome#index'
 
-  devise_scope :caregiver do
-    authenticated  do
-      root to: 'caregivers/caregivers#index', as: 'authenticated_caregiver_root'
-    end
-  end
+  devise_for :caregivers
+  devise_for :admins
 
-  namespace :caregivers do
-  end
-
-  # devise_for :admins
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
-  }
-  devise_scope :admin do
-    authenticated  do
-      root to: 'admins/settings#index', as: 'authenticated_admin_root'
-    end
-  end
+  resources :caregivers
 
   namespace :admins do
     resources :settings
